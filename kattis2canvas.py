8fe2a36b91590ee93cd9947ce9acf26f2053ed52
daman-150191 <100643269+daman-150191@users.noreply.github.com>
2023-02-25 08:26:07 -0800

    Kattismodule (#2)
    * add email code to kattis links
    
    * Added module creation functionality to publish assignments
    
    * Updated web api to python api for canvas

--- kattis2canvas.py
+++ kattis2canvas.py
@@ -12,7 +12,7 @@ import requests
 import requests.cookies
 import requests.exceptions
 from bs4 import BeautifulSoup
-from canvasapi import Canvas
+from canvasapi import Canvas, module
 from canvasapi.course import Course
 from canvasapi.user import User
 
@@ -254,18 +254,29 @@ def course2canvas(offering, canvas_course, dryrun, force, add_to_module):
         if ag.name == 'kattis':
             kattis_group = ag
             break
-
     if not kattis_group:
-        error(f"no kattis assignment group in {canvas_course}")
-        exit(4)
+        # create assignment group if not present on canvas
+        if dryrun:
+            info(f"would create assignment group {kattis_group}.")
+        else:
+            kattis_group = course.create_assignment_group(name='kattis')
+            info(f"created assignment group {kattis_group}.")
 
     if add_to_module:
         modules = {m.name: m for m in course.get_modules()}
         if add_to_module in modules:
             add_to_module = modules[add_to_module]
         else:
-            error(f'could not find {add_to_module} in {modules.keys()}')
-            exit(4)
+            if dryrun:
+                info(f"would create and publish {add_to_module}.")
+            else:
+                args = {"name": add_to_module}
+                add_to_module = course.create_module(module=args)
+                info(f"created module {add_to_module}.")
+
+                args = {'published': "true"}
+                add_to_module.edit(module=args)
+                info(f"published module {add_to_module}.")
 
     canvas_assignments = {a.name: a for a in course.get_assignments(assignment_group_id=kattis_group.id)}
 
@@ -511,13 +522,12 @@ def sendemail(canvas_course):
         if not is_student_enrollment(link.canvas_user):
             continue
         if not link.kattis_user:
-            args = {'access_token': config.canvas_token, 'recipients[]': link.canvas_user.id,
-                    'subject': 'Reminder: Add kattis link in profile',
-                    'body': "Hello " + link.canvas_user.name + "\n\n Please add the missing kattis link in bio for "
-                                                               "course " + canvas_course + "."}
-            rsp = requests.post(config.canvas_url + "api/v1/conversations", data=args)
-            if rsp.status_code != 201:
-                error(f"Kattis login failed. Status: {rsp.status_code}")
+            canvas.create_conversation(recipients=link.canvas_user.id,
+                                       body="Hello " + link.canvas_user.name + "\n\n\n Please add the missing kattis "
+                                                                               "link in bio for "
+                                                                               "course " + canvas_course + ".",
+                                       subject='Reminder: Add kattis link in profile')
+            info(f"Able to send conversation to : {link.canvas_user.id}")
 
 
 if __name__ == "__main__":
